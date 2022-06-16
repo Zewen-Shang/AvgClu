11f741ba07be68960f38133443a62eee6b3d54a6
Zewen-Shang <501206193@qq.com>
2022-06-17 06:22:55 +0800

    first commit

---
+++ VertexClustering.cpp
@@ -0,0 +1,16 @@
+﻿// VertexClustering.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Model.h"
+
+using namespace std;
+int main()
+{
+    Model m("box100k");
+    int sizes[3] = { 40,5, 10 };
+    m.build(sizes);
+    m.addQ();
+    m.getBestPos();
+    m.outputVtk("box100k");
+}
